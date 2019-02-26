ebd65a07aa458ce7c84a18c2d79e129e679e6010
Dennis Enwiya <dennisenwiya@gmail.com>
2019-02-26 12:27:39 -0700

    done

--- front/src/containers/EditFormContainer.js
+++ front/src/containers/EditFormContainer.js
@@ -2,19 +2,39 @@ import EditForm from '../Component/EditForm'
 import { updateNotes } from '../actions/notesActions';
 import { connect } from 'react-redux';
 
-const mapStateToProps = ({notes},props) => {
-  console.log("efc notes", notes)
-  return !notes?{note:{}}:{
+const mapStateToProps = ({ notes }, props) => {
+  return {
     note: notes.notes.filter(note => note._id == props.match.params.id)[0]
   }
 }
 const mapDispatchToProps = dispatch => {
-  
+
   return {
-    dispatchUpdateNote: (_id)=>{
-      dispatch(updateNotes(_id));
+    dispatchUpdateNote: (_id, note) => {
+      dispatch(updateNotes(_id, note));
     }
   }
 }
-
 export default connect(mapStateToProps, mapDispatchToProps)(EditForm)
+
+
+// import EditForm from '../Component/EditForm'
+// import { updateNotes } from '../actions/notesActions';
+// import { connect } from 'react-redux';
+
+// const mapStateToProps = ({notes},props) => {
+//   console.log("efc notes", notes)
+//   return !notes?{note:{}}:{
+//     note: notes.notes.filter(note => note._id == props.match.params.id)[0]
+//   }
+// }
+// const mapDispatchToProps = dispatch => {
+  
+//   return {
+//     dispatchUpdateNote: (_id)=>{
+//       dispatch(updateNotes(_id));
+//     }
+//   }
+// }
+
+// export default connect(mapStateToProps, mapDispatchToProps)(EditForm)
